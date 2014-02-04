b2da4974ce59ad61b953dcdd175d6438c895bd7f
Kodekpl <Kodekgames@gmail.com>
2014-02-04 14:35:51 +0100

    Created FlowerPot block class for setting and getting flower pot contents. Fixes BUKKIT-5368.
    With new flowers in Minecraft 1.7, Mojang changed the way content of
    flower pot is saved. This made it impossible to get and set flower pot
    content correctly.
    
    With this additional class, plugins can get and set flower pot content
    correctly.

---
+++ src/main/java/org/bukkit/craftbukkit/block/CraftFlowerPot.java
@@ -0,0 +1,46 @@
+package org.bukkit.craftbukkit.block;
+
+import net.minecraft.server.TileEntityFlowerPot;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.FlowerPot;
+import org.bukkit.craftbukkit.CraftWorld;
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+
+public class CraftFlowerPot extends CraftBlockState implements FlowerPot {
+    private final TileEntityFlowerPot flowerPot;
+    private ItemStack contents;
+
+    public CraftFlowerPot(Block block) {
+        super(block);
+
+        flowerPot = (TileEntityFlowerPot) ((CraftWorld) block.getWorld()).getTileEntityAt(getX(), getY(), getZ());
+        contents = CraftItemStack.asNewCraftStack(flowerPot.a());
+        contents.setDurability((short) flowerPot.b());
+    }
+
+    @Override
+    public boolean update(boolean force, boolean applyPhysics) {
+        boolean result = super.update(force, applyPhysics);
+
+        if (result) {
+            flowerPot.a(CraftItemStack.asNMSCopy(contents).getItem(), contents.getDurability());
+            flowerPot.update();
+        }
+
+        return result;
+    }
+
+    public ItemStack getContents() {
+        return contents;
+    }
+
+    public void setContents(ItemStack itemStack) {
+        if (itemStack == null) {
+            itemStack = new ItemStack(Material.AIR);
+        }
+        contents = itemStack;
+    }
+}
