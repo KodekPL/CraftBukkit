0751976901c27d4758d6fde4238a66b72bf03b10
Kodek <Kodekgames@gmail.com>
2014-03-14 14:27:50 +0100

    Changed flowerpot contents as ItemStack to MaterialData

--- src/main/java/org/bukkit/craftbukkit/block/CraftFlowerPot.java
+++ src/main/java/org/bukkit/craftbukkit/block/CraftFlowerPot.java
@@ -8,17 +8,18 @@ import org.bukkit.block.FlowerPot;
 import org.bukkit.craftbukkit.CraftWorld;
 import org.bukkit.craftbukkit.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.MaterialData;
 
 public class CraftFlowerPot extends CraftBlockState implements FlowerPot {
     private final TileEntityFlowerPot flowerPot;
-    private ItemStack contents;
+    private MaterialData contents;
 
     public CraftFlowerPot(Block block) {
         super(block);
 
         flowerPot = (TileEntityFlowerPot) ((CraftWorld) block.getWorld()).getTileEntityAt(getX(), getY(), getZ());
-        contents = CraftItemStack.asNewCraftStack(flowerPot.a());
-        contents.setDurability((short) flowerPot.b());
+        contents = CraftItemStack.asNewCraftStack(flowerPot.a()).getData();
+        contents.setData((byte) flowerPot.b());
     }
 
     @Override
@@ -26,26 +27,22 @@ public class CraftFlowerPot extends CraftBlockState implements FlowerPot {
         boolean result = super.update(force, applyPhysics);
 
         if (result) {
-            if (contents == null) {
-                flowerPot.a(null, -1);
-            } else {
-                flowerPot.a(CraftItemStack.asNMSCopy(contents).getItem(), contents.getDurability());
-            }
+            flowerPot.a(CraftItemStack.asNMSCopy(contents.toItemStack(1)).getItem(), contents.getData());
             flowerPot.update();
         }
 
         return result;
     }
 
-    public ItemStack getContents() {
+    public MaterialData getContents() {
         return contents;
     }
 
-    public void setContents(ItemStack itemStack) {
-        if (itemStack == null || itemStack.getType() == Material.AIR) {
-            contents = null;
+    public void setContents(MaterialData materialData) {
+        if (materialData == null) {
+            contents = new MaterialData(Material.AIR);
         } else {
-            contents = itemStack;
+            contents = materialData;
         }
     }
 }
